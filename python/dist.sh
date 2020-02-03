95222f318c267fc1370e2ab9cdc706dd92c5691a
Niklas Salmoukas <niklas@salmoukas.com>
2020-02-03 13:24:07 +0100

    add dist script

---
+++ python/dist.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+ROOT="$(realpath "$( dirname "${BASH_SOURCE[0]}" )")"
+
+# check tools
+if ! which pyscriptpacker > /dev/null;
+then
+    echo "Install pyscriptpacker: npm install -g pyscriptpacker"
+    exit 1
+fi
+
+# clear dist directory
+rm -rf "$ROOT/dist"
+mkdir -p "$ROOT/dist"
+
+# pack extensions
+pyscriptpacker "$ROOT/dist/assetimport.blender.py" "$ROOT/extensions/assetimport/blender" "$ROOT/libraries"
