4fa9da28b7c1512124b33b8459f3d7e36d89e2a7
Aleks <crni.aki@gmail.com>
2020-03-21 20:32:00 +0100

    Houdini HDRI and Surfaces

--- py/dist_exts.sh
+++ py/dist_exts.sh
@@ -12,6 +12,7 @@ mkdir -p "$ROOT/dist"
 npx -q pyscriptpacker@latest "3.5" "$ROOT/dist/assetimport_blender.py" "assetninja_assetimport" "assetimport_blender" "$ROOT/exts" "$ROOT/libs"
 npx -q pyscriptpacker@latest "2.7" "$ROOT/dist/assetimport_maya.py" "assetninja_assetimport" "assetimport_maya" "$ROOT/exts" "$ROOT/libs"
 npx -q pyscriptpacker@latest "2.7" "$ROOT/dist/assetimport_c4d.pyp" "assetninja_assetimport" "assetimport_c4d" "$ROOT/exts" "$ROOT/libs"
+npx -q pyscriptpacker@latest "2.7" "$ROOT/dist/assetimport_houdini.py" "assetninja_assetimport" "assetimport_houdini" "$ROOT/exts" "$ROOT/libs"
 
 # pack spaceprobe extensions
 npx -q pyscriptpacker@latest "3.5" "$ROOT/dist/spaceprobe_blender.py" "assetninja_spaceprobe" "spaceprobe_blender" "$ROOT/exts" "$ROOT/libs"
---
+++ py/exts/assetimport_houdini/importer/environment_hdri.py
@@ -0,0 +1,49 @@
+import re
+import hou
+import assetexchange_shared
+
+def create_dome(name, path,renderer):
+    # Initialize parent node variable.
+    if locals().get("hou_parent") is None:
+        hou_parent = hou.node("/obj")
+    
+    light_setup = hou_parent.createNode("envlight", name, run_init_scripts=False, 
+                                        load_contents=True, exact_type_name=True)
+    hou_parm = light_setup.parm("env_map")
+    hou_parm.lock(False)
+    hou_parm.set(path)
+    hou_parm.setAutoscope(False)
+
+def environment_hdri(asset, selectedVariants):
+    # explode variants
+    variantLabels, variantConfigs = assetexchange_shared.asset.explode_variants(
+        'Primary', selectedVariants)
+
+    # iterate variant config
+    for variantConfig in variantConfigs:
+
+        # get environment map
+        object_list = assetexchange_shared.asset.filter_objects_by_variant_config(
+            asset, 'Primary', variantLabels, variantConfig)
+        if len(object_list) == 0:
+            return
+        env_map = object_list[0]
+
+        # extract resolution
+        resolution = 4 * 1000
+
+        if len(env_map['variants']) > 0:
+            if 'Resolution' in env_map['variants'][0]:
+                res_str = env_map['variants'][0]['Resolution']
+                if re.match("^[0-9]+[kK]$", res_str):
+                    resolution = int(res_str[:-1]) * 1000
+                if re.match("^[0-9]+$", res_str):
+                    resolution = int(res_str)
+
+        # name prefix
+
+        asset_name = asset['uid']
+        path_to_asset = env_map["file"]["path"]
+        asset_name = asset_name.split(".")[1]
+        renderer_setup = "mantra"
+        create_dome(asset_name,path_to_asset,renderer_setup)
